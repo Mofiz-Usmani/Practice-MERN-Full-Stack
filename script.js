2feaa97d2391c49b2174d69bd1ef011cbd671da4
Jackal <mofizusmani1@gmail.com>
2025-10-05 15:43:47 +0530

    rest method

--- script.js
+++ script.js
@@ -974,7 +974,29 @@
 
 // console.log(..."apnacollege");
 
-let arr = [1,2,3,4,5];
+// let arr = [1,2,3,4,5];
 
-let newArr = [...arr];
-console.log(newArr);
+// let newArr = [...arr];
+// console.log(newArr);
+
+
+
+// let odd = [1,3,5,7,9];
+// let even = [2,4,6,8];
+
+// let nums = [...odd,...even];
+// console.log(nums);
+
+
+
+
+
+
+
+
+// Rest Method : lets you collect multiple arguments into a single array
+function sum(...numbers){
+    return numbers.reduce((acc, num) => acc + num, 0);
+}
+
+console.log(sum(1,2,3,4,5));
