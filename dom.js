8ef72752dce9257731b11d250e739fb43c20eff8
Jackal <mofizusmani1@gmail.com>
2025-10-05 17:24:10 +0530

    getElementsByClassName

--- dom.js
+++ dom.js
@@ -1,9 +1,20 @@
-let imgObj = document.getElementById("mainImg");
+// let imgObj = document.getElementById("mainImg");
 
-console.log(imgObj.src);
+// console.log(imgObj.src);
 
-// Now changing the image src with new image usign get element by id
+// // Now changing the image src with new image usign get element by id
 
-imgObj.src = "https://images.unsplash.com/photo-1759431318039-0666f33d504b?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+// imgObj.src = "https://images.unsplash.com/photo-1759431318039-0666f33d504b?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
 
-console.log(imgObj.src);
+// console.log(imgObj.src);
+
+
+
+
+let images = document.getElementsByClassName("imgs");
+
+for(let i=0; i<images.length; i++){
+    console.dir(document.getElementsByClassName("img")[i]);
+    console.log(images[i].src);
+}
+
