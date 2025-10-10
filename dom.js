181aa58c1784af9122e94f67ed1ace97e33042f6
Jackal <mofizusmani1@gmail.com>
2025-10-10 15:04:56 +0530

    async function

--- dom.js
+++ dom.js
@@ -856,30 +856,44 @@
 
 
 
-let h1 = document.querySelector('h1');
+// let h1 = document.querySelector('h1');
 
-function changeColor(color, delay){
-  return new Promise((resolve, reject) => {
-    setTimeout(() =>{
-      h1.style.color = color;
-      resolve("color changed!");
-    }, delay);
-  });
+// function changeColor(color, delay){
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() =>{
+//       h1.style.color = color;
+//       resolve("color changed!");
+//     }, delay);
+//   });
+// }
+
+// changeColor('red', 1000)
+// .then(() => {
+//   console.log("red color applied");
+//   return changeColor('orange', 1000);
+// })
+// .then(() => {
+//   console.log("orange color applied");
+//   return changeColor('green', 1000);
+// })
+// .then(() => {
+//   console.log("green color applied");
+//   return changeColor('yellow', 1000);
+// })
+// .then(() => {
+//   console.log("yellow color applied");
+// });
+
+
+
+
+
+
+
+
+
+async function greet() {
+  return "hello";
 }
 
-changeColor('red', 1000)
-.then(() => {
-  console.log("red color applied");
-  return changeColor('orange', 1000);
-})
-.then(() => {
-  console.log("orange color applied");
-  return changeColor('green', 1000);
-})
-.then(() => {
-  console.log("green color applied");
-  return changeColor('yellow', 1000);
-})
-.then(() => {
-  console.log("yellow color applied");
-});
+console.log(greet());
