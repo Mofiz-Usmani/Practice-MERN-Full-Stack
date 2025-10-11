157831658971e65029366f6a6f56ce881c12d52e
Jackal <mofizusmani1@gmail.com>
2025-10-11 10:38:58 +0530

    handling rejections with await

--- dom.js
+++ dom.js
@@ -974,6 +974,42 @@
 
 
 
+let h1 = document.querySelector('h1');
+
+function changeColor(color, delay){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let num = Math.floor(Math.random() *6);
+            if(num>3) {
+                reject("Promise Rejected");
+            }
+            h1.style.color = color;
+            console.log(`Color changed to ${color}`);
+            resolve("Color Changed");
+        }, delay);
+    });
+}
+
+async function demo() {
+    try {
+        await changeColor('red', 1000);
+        await changeColor('green', 1000);
+        await changeColor('yellow', 1000);
+        await changeColor('pink', 1000);
+        await changeColor('orange', 1000);
+        changeColor('brown', 1000);
+    } catch(err) {
+        console.log("Error caught");
+        console.log(err);
+    }
+    let a = 5;
+    console.log(a);
+    console.log("new number = ", a);
+}
+
+demo();
+
+
 
 
 
