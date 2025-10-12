7b5cb864db540e3ecd7b0a95a7e9aa3e58dca9fe
Jackal <mofizusmani1@gmail.com>
2025-10-12 18:34:43 +0530

    Filter method

--- practice.js
+++ practice.js
@@ -65,11 +65,44 @@
 // });
 
 
-let arr = [1,2,3,4,5];
+// let arr = [1,2,3,4,5];
 
-arr.forEach((num, i) => {
-    arr[i] = num*2;
-});
+// arr.forEach((num, i) => {
+//     arr[i] = num*2;
+// });
 
-console.log(arr);
+// console.log(arr);
 // changes the orignal array
+
+
+
+
+
+
+
+// let arr = [1,2,3,4,5];
+
+// let newArr = arr.map((el) => {
+//     return el*2;
+// });
+
+// console.log(newArr); // changes done in new array 
+// console.log(arr); // orignal remains the same
+
+
+
+
+
+
+
+let nums = [1,2,3,4,5,6,7,8,9];
+
+// let even = nums.filter((num) => {
+//     return num%2 == 0;
+// });
+
+// console.log(even);
+
+let even = nums.filter((num) => (num%2 == 0));
+
+console.log(even);
