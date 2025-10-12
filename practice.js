bdfa6f6f04c6376a8123032582c2b19ec03341c1
Jackal <mofizusmani1@gmail.com>
2025-10-12 18:55:15 +0530

    spread in js

--- practice.js
+++ practice.js
@@ -137,7 +137,23 @@
 
 
 
-let arr = [1,2,3,4,5];
+// let arr = [1,2,3,4,5];
 
-let total = arr.reduce((a,b) => a*b);
-console.log(total);
+// let total = arr.reduce((a,b) => a*b);
+// console.log(total);
+
+
+
+
+
+let arr1 = [1,2,3,4,5];
+
+let arr2 = [...arr1,6,7,8,9];
+
+console.log(arr1)
+console.log(arr2)
+
+console.log(Math.max(...arr1))
+console.log(Math.min(...arr2))
+
+console.log(...arr1,...arr2);
