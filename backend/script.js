fe786c9a14ca482bd4624b44486285764b2a915f
Jackal <mofizusmani1@gmail.com>
2025-10-20 08:40:11 +0530

    node backend

--- backend/script.js
+++ backend/script.js
@@ -3,8 +3,20 @@
 //     console.log("Hello", i);
 // }
 
-let args = process.argv;
+// let args = process.argv;
 
-for(let i=2; i<args.length; i++){
-    console.log("Hello to ",args[i]);
-}
+// for(let i=2; i<args.length; i++){
+//     console.log("Hello to ",args[i]);
+// }
+
+
+// let a = Number(process.argv[2]);
+// let b = Number(process.argv[3]);
+
+// console.log(a + b);
+
+
+
+const someValue = require("./math");
+
+console.log(someValue);
