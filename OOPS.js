abbd2c35989e02ad74d8161f847610fa3c4a58f1
Jackal <mofizusmani1@gmail.com>
2025-10-22 00:03:13 +0530

    OOPs in js

--- OOPS.js
+++ OOPS.js
@@ -1,1 +1,10 @@
-console.log("This is OOPS.js file");
+class Animal {
+  constructor(name, species) {
+    this.name = name;
+    this.species = species;
+  }
+
+  makeSound() {
+    console.log(`${this.name} makes a sound.`);
+  }
+}
