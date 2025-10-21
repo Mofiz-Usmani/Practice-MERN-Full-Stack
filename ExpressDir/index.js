f4309c5be33833a08e6f660d2cb15fcbf130f3a6
Jackal <mofizusmani1@gmail.com>
2025-10-21 09:36:29 +0530

    backend

--- ExpressDir/index.js
+++ ExpressDir/index.js
@@ -1,30 +1,25 @@
 const express = require("express");
 const app = express();
 
-let port = 3000;
+const port = 3000;
 
-// Define routes first
+
 app.get("/", (req, res) => {
-    res.send("Hello World");
+  res.send("Hello World");
 });
 
 app.get("/about", (req, res) => {
-    res.send("About Us");
+  res.send("About Us");
 });
 
 app.get("/apple", (req, res) => {
-    res.send("Apple Page");
+  res.send("Apple Page");
 });
 
 app.get("/banana", (req, res) => {
-    res.send("Banana Page");
-});
-
-// Catch-all route for anything else
-app.use((req, res) => {
-    res.send("Ka Bau");
+  res.send("Banana Page");
 });
 
 app.listen(port, () => {
-    console.log(`App is running on port ${port}`);
+  console.log(`App is running on port ${port}`);
 });
