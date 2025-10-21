426ab786014bac3940e86d51fa24f243607cd959
Jackal <mofizusmani1@gmail.com>
2025-10-22 00:04:21 +0530

    OOPs in js

--- OOPS.js
+++ OOPS.js
@@ -8,3 +8,10 @@ class Animal {
     console.log(`${this.name} makes a sound.`);
   }
 }
+
+
+// new operator is used to create an instance of the class
+const dog = new Animal("Buddy", "Dog");
+dog.makeSound(); // Output: Buddy makes a sound.
+const cat = new Animal("Whiskers", "Cat");
+cat.makeSound(); // Output: Whiskers makes a sound.
