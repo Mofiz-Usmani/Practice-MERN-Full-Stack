5281ae2110ea4f0540bd3b0fb1181708cd2fd25e
Jackal <mofizusmani1@gmail.com>
2025-10-23 18:44:47 +0530

    oops

--- OOPS.js
+++ OOPS.js
@@ -486,24 +486,35 @@
 
 
 // Observer Pattern Example
-class Subject {
-  constructor() {
-    this.observers = [];
-  }
-    subscribe(observer) {
-    this.observers.push(observer);
-    }
-    unsubscribe(observer) {
-    this.observers = this.observers.filter(obs => obs !== observer);
-    }
-    notify(data) {
-        this.observers.forEach(observer => observer.update(data));
-    }
-}
+// class Subject {
+//   constructor() {
+//     this.observers = [];
+//   }
+//     subscribe(observer) {
+//     this.observers.push(observer);
+//     }
+//     unsubscribe(observer) {
+//     this.observers = this.observers.filter(obs => obs !== observer);
+//     }
+//     notify(data) {
+//         this.observers.forEach(observer => observer.update(data));
+//     }
+// }
 
-class Observer {
-    update(data) {
-        console.log("Observer received data:", data);
-    }
-}
+// class Observer {
+//     update(data) {
+//         console.log("Observer received data:", data);
+//     }
+// }
+
+
+const subject = new Subject();
+const observer1 = new Observer();
+const observer2 = new Observer();
+
+
+
+
+
+
 
