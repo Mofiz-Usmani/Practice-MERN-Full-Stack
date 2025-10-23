63fcf3b595e759d5907caae776f5298147abe031
Jackal <mofizusmani1@gmail.com>
2025-10-23 18:38:44 +0530

    oops

--- OOPS.js
+++ OOPS.js
@@ -335,22 +335,27 @@
 
 
 // Event Emitter Example
-class EventEmitter {
-  constructor() {
-    this.events = {};
-  }
-    on(event, listener) {   
-    if (!this.events[event]) {
-        this.events[event] = [];    
-    }
-    this.events[event].push(listener);
-  }
+// class EventEmitter {
+//   constructor() {
+//     this.events = {};
+//   }
+//     on(event, listener) {   
+//     if (!this.events[event]) {
+//         this.events[event] = [];    
+//     }
+//     this.events[event].push(listener);
+//   }
 
-  emit(event, ...args) {
-    if (this.events[event]) {
-      this.events[event].forEach(listener => listener(...args));
-    }
-  }
-}
+//   emit(event, ...args) {
+//     if (this.events[event]) {
+//       this.events[event].forEach(listener => listener(...args));
+//     }
+//   }
+// }
 
 
+// const emitter = new EventEmitter();
+// emitter.on("greet", name => {
+//   console.log(`Hello, ${name}!`);
+// });
+// emitter.emit("greet", "Alice");
