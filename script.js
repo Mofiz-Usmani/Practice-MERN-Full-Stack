11e16cb251b92e034713421f544632bbb59b5b29
Jackal <mofizusmani1@gmail.com>
2025-11-07 17:15:53 +0530

    every in js

--- script.js
+++ script.js
@@ -1127,6 +1127,18 @@
 
 
 
+// let numbers = [1, 2, 3, 4, 5];
+// let hasEven = numbers.some(num => num % 2 === 0);
+// console.log(hasEven); 
+
+
+
+
+
+
+
+
+
 let numbers = [1, 2, 3, 4, 5];
-let hasEven = numbers.some(num => num % 2 === 0);
+let hasEven = numbers.every(num => num % 2 === 0);
 console.log(hasEven); 
